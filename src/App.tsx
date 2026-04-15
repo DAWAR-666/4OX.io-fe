958cd2da997bde41ed711aa4f6920fabe42710c2
DAWAR <dawarpratham8@gmail.com>
2026-04-15 13:48:39 +0530

    created body route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Body from "./components/Body"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body/>}>
+
+          </Route>
+        </Routes>  
+      </BrowserRouter>
+    </div>
   )
 }
 
