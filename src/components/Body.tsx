98eca3644f5f52ac8df3fb92d2ce6bf5deee5d78
DAWAR <dawarpratham8@gmail.com>
2026-04-15 17:45:48 +0530

    created landing page

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -18,7 +18,40 @@ const Body = () => {
     
     <div className="bg-black/70 w-screen min-h-screen overflow-hidden ">
         <AnimatedBackground/>
-        <div className=""></div>
+        <div className="h-screen flex justify-center items-center z-1 ">
+            <div className="flex flex-col bg-black/50 text-white p-30">
+                <span className="text-7xl md:text-8xl lg:text-9xl font-extrabold animate-fade-in ">
+                    4OX.io
+                </span>
+                <span className="text-center text-xl font-black text-[#8df0cc] ">
+                    Tic-Tac-Toe but with only 4 pieces at time
+                </span>
+                {!playChoices&&
+                <button 
+                    className="border-2 border-[#8df0cc] w-1/2 mx-auto mt-5 animate-fade-in text-4xl py-2 rounded-4xl bg-[#0d63f8] hover:bg-[#ff0088] cursor-pointer transition-colors font-extrabold"
+                    onClick={handlePlay}
+                >
+                    PLAY
+                </button>}
+                {
+                    playChoices &&
+                    <>
+                    <div className="gap-2 w-full flex justify-center mt-5">
+                    <button className="border-2 border-[#8df0cc] w-1/2 mx-auto animate-fade-in text-3xl p-2 rounded-4xl bg-[#0d63f8] hover:bg-[#ff0088] cursor-pointer transition-colors font-extrabold"
+                    >CREATE</button>
+                    <button className="border-2 border-[#8df0cc] w-1/2 mx-auto animate-fade-in text-3xl p-2 rounded-4xl bg-[#0d63f8] hover:bg-[#ff0088] cursor-pointer transition-colors font-extrabold"
+                    >JOIN</button>
+                    </div>
+                    <div className="w-full justify-center flex">
+                        <button className="border-2 border-[#8df0cc] animate-fade-in text-xl p-2 rounded-4xl bg-[#0d63f8] hover:bg-[#ff0088] cursor-pointer transition-colors font-extrabold"
+                        onClick={()=>{setPlayChoices(false)}}
+                    >BACK</button>
+                    </div>
+                    </>
+                }
+            </div>
+            
+        </div>
     </div>
   )
 }
