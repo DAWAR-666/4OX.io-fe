0a164b0ed1a178ce6b9058c7b2ff045671415d0d
DAWAR <dawarpratham8@gmail.com>
2026-04-15 20:18:52 +0530

    added roomStore in zustand

--- src/utils/authStore.ts
+++ src/utils/authStore.ts
@@ -11,11 +11,26 @@ type User={
         winRate: number;
     }
 }
+type Room={
+    roomId:string,
+    players:string[],
+    gameState:string,
+    winner:string|null
+}
+
 type AuthStore={
     user:User|null,
     setUser:(user:User|null)=>void
 }
+type RoomStore={
+    room:Room|null,
+    setRoom:(room:Room|null)=>void
+}
 export const useAuthStore=create<AuthStore>((set)=>({
     user:null,
     setUser:(user)=>set({user})
+}))
+export const useRoomStore=create<RoomStore>((set)=>({
+    room:null,
+    setRoom:(room)=>set({room})
 }))
