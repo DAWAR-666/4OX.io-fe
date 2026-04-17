f2aa85b73346c8a4b3cb3f759231334d3fec0aef
DAWAR <dawarpratham8@gmail.com>
2026-04-17 23:37:27 +0530

    installed font and working on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,17 @@ import axios from "axios"
 
 const Header = () => {
     const setUser=useAuthStore(state=>state.setUser)
+    const user=useAuthStore(state=>state.user)
+    console.log(user)
     const apiUrl=import.meta.env.VITE_BASE_URL
     const handleLogout=async()=>{
         setUser(null)
         await axios.post(`${apiUrl}/auth/logout`,{},{withCredentials:true})
     }
   return (
-    <div>
-        <button className="fixed border-2 text-white text-4xl hover:bg-amber-300 "
+    <div className="text-white bg-white/75 w-full fixed">
+      {user!.userName}
+        <button className="border-2 text-white text-4xl hover:bg-amber-300 "
         onClick={handleLogout}
         >logout</button>
     </div>
